1f46a42d6fabc9b3058cf7152f46a1722482dd34
szr712 <zirui990712@163.com>
2021-04-25 16:19:25 +0800

    更改参数

--- analyze.py
+++ analyze.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 dataSet = "./data"
 className = "PostionStablity"
-modelName = "./model\PostionStablity\初学者位置稳定性_Dense1_原train_不扩容_不固定_batch小_0.636__20210425_07_58_11.h5"
+modelName = "./model\PostionStablity\初学者位置稳定性_Dense1_原train_不扩容_不固定_batch小_0.545__20210425_08_12_14.h5"
 
 
 def plot_with_labels(lowDWeights, labels, list):
