1913e0a98513f284b33b70337f250d796adb639b
szr712 <zirui990712@163.com>
2021-04-25 12:34:26 +0800

    修改参数

--- beginnerModel.py
+++ beginnerModel.py
@@ -15,7 +15,7 @@ from tensorflow.python.keras.utils.vis_utils import plot_model
 from dataReader import padding, load_dataset_beginner, load_dataset_beginner_reg
 from datetime import datetime
 
-modelName = "初学者位置稳定性_Dense1_扩容_不固定_"
+modelName = "初学者位置稳定性_Dense1_小扩容_不固定_"
 
 # os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 epochs, batch_size = 200, 512
