f6ff8a2888bc900d6786eb32fe072d4cb78dd002
szr712 <zirui990712@163.com>
2021-04-28 15:06:22 +0800

    更改参数

--- beginnerModel.py
+++ beginnerModel.py
@@ -18,7 +18,7 @@ from datetime import datetime
 modelName = "初学者动作标准度_Dense1_正常数据集_扩容_不固定_"
 
 # os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-epochs, batch_size = 200, 64
+epochs, batch_size = 200, 32
 dataSet = "./data"
 className = "RopeSwinging"
 logDir = "./logs"
