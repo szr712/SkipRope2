e9c77d904f65f002cc24a4185bb7b8ee29f9c1ab
szr712 <zirui990712@163.com>
2021-04-28 10:55:38 +0800

    训练部分包含50%测试

--- beginnerModel.py
+++ beginnerModel.py
@@ -15,7 +15,7 @@ from tensorflow.python.keras.utils.vis_utils import plot_model
 from dataReader import padding, load_dataset_beginner, load_dataset_beginner_reg
 from datetime import datetime
 
-modelName = "初学者位置稳定性_Dense1_训练部分包含测试_扩容_不固定_"
+modelName = "初学者位置稳定性_Dense1_训练部分包含50测试_扩容_不固定_"
 
 # os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 epochs, batch_size = 200, 256
