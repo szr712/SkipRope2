15974c08cc29f150b64129a274743f7c0f4afc2e
szr712 <zirui990712@163.com>
2021-05-07 14:52:11 +0800

    初学者位置稳定性_Dense1_无扩容_有finetuning_更改归一化_

--- beginnerModel.py
+++ beginnerModel.py
@@ -15,7 +15,7 @@ from tensorflow.python.keras.utils.vis_utils import plot_model
 from dataReader import padding, load_dataset_beginner, load_dataset_beginner_reg
 from datetime import datetime
 
-modelName = "初学者位置稳定性_Dense1_有扩容_有finetuning_更改归一化_"
+modelName = "初学者位置稳定性_Dense1_无扩容_有finetuning_更改归一化_"
 
 # os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 epochs, batch_size = 200, 256
@@ -171,8 +171,7 @@ def train_model(model, trainX, trainy, testX, testy, class_weights):
 
 if __name__ == "__main__":
     print(modelName)
-    X_train, X_test, y_train, y_test, class_weights, _ = load_dataset_beginner(dataSet, className, augment=True,
-                                                                               times=150)
+    X_train, X_test, y_train, y_test, class_weights, _ = load_dataset_beginner(dataSet, className)
     model = postion_model()
     model.summary()
     # model = postion_model()
